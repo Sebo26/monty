b86997908fd89eee948bbfd82fe8baaf45d60c41
root <sebomaduna26@gmail.com>
2023-12-17 07:53:20 -0800

    Pop

---
+++ pop.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * pop - the function removes the top element of the stack.
+ * Return: value that will be deleted.
+ */
+int pop(stack_t **stack)
+{
+	int n;
+	static unsigned int line_number = 1;
+	stack_t *temp = *stack;
+	if(isEmpty(*stack))
+	{
+		fprintf(stderr, "L%d: can't pop, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	n = (*stack)->n;
+
+	*stack = (*stack)->next;
+	free(temp);
+	temp = NULL;
+	line_number++;
+	return n;
+}
